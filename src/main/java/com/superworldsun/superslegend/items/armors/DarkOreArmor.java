8963433b9c887b66b7e617bec3e274cc20004a5f
superworldsun <superworldsun@gmail.com>
2024-06-08 05:35:23 -0400

    Add Dark Ore item, dark ore, Dark ore tools, Dark ore armor items

---
+++ src/main/java/com/superworldsun/superslegend/items/armors/DarkOreArmor.java
@@ -0,0 +1,53 @@
+package com.superworldsun.superslegend.items.armors;
+
+import com.superworldsun.superslegend.items.customclass.NonEnchantArmor;
+import com.superworldsun.superslegend.registries.ItemInit;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ArmorMaterial;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import software.bernie.geckolib.animatable.GeoItem;
+import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
+import software.bernie.geckolib.core.animatable.instance.SingletonAnimatableInstanceCache;
+import software.bernie.geckolib.core.animation.*;
+import software.bernie.geckolib.core.object.PlayState;
+
+public class DarkOreArmor extends NonEnchantArmor implements GeoItem {
+    private AnimatableInstanceCache cache = new SingletonAnimatableInstanceCache(this);
+    public DarkOreArmor(ArmorMaterial material, Type type, Properties properties) {
+        super(material, type, properties);
+    }
+
+    private PlayState predicate(AnimationState animationState){
+        animationState.getController().setAnimation(RawAnimation.begin().then("idle", Animation.LoopType.LOOP));
+        return PlayState.CONTINUE;
+    }
+
+    @Override
+    public void registerControllers(AnimatableManager.ControllerRegistrar controllerRegistrar) {
+        controllerRegistrar.add(new AnimationController(this, "controller", 0, this::predicate));
+    }
+
+    @Override
+    public AnimatableInstanceCache getAnimatableInstanceCache() {
+        return cache;
+    }
+
+    @Override
+    public void onArmorTick(ItemStack stack, Level level, Player player) {
+        super.onArmorTick(stack, level, player);
+        if (!level.isClientSide){
+            boolean isHelmetOn = player.getItemBySlot(EquipmentSlot.HEAD).getItem() == ItemInit.DARK_HELMET.get();
+            boolean isChestplateOn = player.getItemBySlot(EquipmentSlot.CHEST).getItem() == ItemInit.DARK_CHESTPLATE.get();
+            boolean isLeggingsOn = player.getItemBySlot(EquipmentSlot.LEGS).getItem() == ItemInit.DARK_LEGGINGS.get();
+            boolean isBootsOn = player.getItemBySlot(EquipmentSlot.FEET).getItem() == ItemInit.DARK_GREAVES.get();
+            if(isHelmetOn&isChestplateOn&isLeggingsOn&isBootsOn && !level.isDay())
+            {
+                player.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 4, 1, false, false, false));
+            }
+        }
+    }
+}
---
+++ src/main/java/com/superworldsun/superslegend/items/item/DarkPickaxe.java
@@ -0,0 +1,31 @@
+package com.superworldsun.superslegend.items.item;
+
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.PickaxeItem;
+import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.Tier;
+import net.minecraft.world.level.Level;
+
+public class DarkPickaxe extends PickaxeItem {
+
+    public DarkPickaxe(Tier pTier, int pAttackDamageModifier, float pAttackSpeedModifier, Properties pProperties) {
+        super(pTier, pAttackDamageModifier, pAttackSpeedModifier, pProperties);
+    }
+
+    @Override
+    public void inventoryTick(ItemStack stack, Level level, Entity entity, int itemSlot, boolean isSelected) {
+        super.inventoryTick(stack, level, entity, itemSlot, isSelected);
+        Player player = (Player) entity;
+        if (player.tickCount % 20 == 0) { // Check if 20 ticks have passed
+            if (level.isDay()) {
+                if (entity instanceof Player && player.getMainHandItem().equals(stack) || player.getOffhandItem().equals(stack)) {
+                    if (level.canSeeSky(player.blockPosition())) {
+                        stack.hurtAndBreak(1, player, (p) -> p.broadcastBreakEvent(player.getUsedItemHand()));
+                    }
+                }
+            }
+        }
+    }
+}
