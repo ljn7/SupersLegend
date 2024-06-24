5314665cefb881d1998f6dd33c890c53c7d2034d
superworldsun <superworldsun@gmail.com>
2024-06-24 03:57:42 -0400

    Ported StrengthHandItem, GoronBracelet, SilverGauntlets, GoldenGauntlets.

---
+++ src/main/java/com/superworldsun/superslegend/items/curios/hands/GoldenGauntlets.java
@@ -0,0 +1,11 @@
+package com.superworldsun.superslegend.items.curios.hands;
+
+import com.superworldsun.superslegend.SupersLegendMain;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = SupersLegendMain.MOD_ID)
+public class GoldenGauntlets extends StrengthHandItem {
+	public GoldenGauntlets() {
+		super(new Properties(), 3);
+	}
+}
---
+++ src/main/java/com/superworldsun/superslegend/items/curios/hands/GoronBracelet.java
@@ -0,0 +1,11 @@
+package com.superworldsun.superslegend.items.curios.hands;
+
+import com.superworldsun.superslegend.SupersLegendMain;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = SupersLegendMain.MOD_ID)
+public class GoronBracelet extends StrengthHandItem {
+	public GoronBracelet() {
+		super(new Properties(), 1);
+	}
+}
