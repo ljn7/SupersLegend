ccc1e13fa8c8cd83b14b5aa6ffb18e640225af02
superworldsun <superworldsun@gmail.com>
2024-06-26 06:15:01 -0400

    Update GiantsMask, now drains magic when used, now checks if player has magic, now scales player slowly between sizes but still wip

--- src/main/java/com/superworldsun/superslegend/items/curios/head/masks/GiantsMask.java
+++ src/main/java/com/superworldsun/superslegend/items/curios/head/masks/GiantsMask.java
@@ -1,8 +1,11 @@
 package com.superworldsun.superslegend.items.curios.head.masks;
 
+import com.superworldsun.superslegend.capability.magic.MagicProvider;
+import com.superworldsun.superslegend.items.item.MagicCape;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
@@ -20,49 +23,152 @@ public class GiantsMask extends Item implements ICurioItem {
     public GiantsMask(Properties pProperties) {
         super(pProperties);
     }
+    private static final float MANA_COST = 0.01F;
+
+
+    //TODO, right now the size doesn't transition when done with curio tick but does with curio equip and un-equip.
+    @Override
+    public void curioTick(String identifier, int index, LivingEntity livingEntity, ItemStack stack) {
+        MagicProvider.spendMagic((Player) livingEntity, MANA_COST);
+        if (!(livingEntity instanceof Player)) {
+            return;}
+        Player player = (Player) livingEntity;
+        boolean hasMagic = MagicProvider.hasMagic(player, MagicCape.MANA_COST);
+
+        if (hasMagic)
+        {
+            ScaleData height = ScaleTypes.HEIGHT.getScaleData(livingEntity);
+            height.setTargetScale(6.22f);
+            height.setScaleTickDelay(height.getScaleTickDelay());
+            ScaleData width = ScaleTypes.WIDTH.getScaleData(livingEntity);
+            width.setTargetScale(6.22f);
+            width.setScaleTickDelay(width.getScaleTickDelay());
+            ScaleData reach = ScaleTypes.BLOCK_REACH.getScaleData(livingEntity);
+            reach.setTargetScale(2f);
+            reach.setScaleTickDelay(reach.getScaleTickDelay());
+            ScaleData ereach = ScaleTypes.ENTITY_REACH.getScaleData(livingEntity);
+            ereach.setTargetScale(2f);
+            ereach.setScaleTickDelay(ereach.getScaleTickDelay());
+            ScaleData motion = ScaleTypes.MOTION.getScaleData(livingEntity);
+            motion.setTargetScale(3f);
+            motion.setScaleTickDelay(motion.getScaleTickDelay());
+            ScaleData jump = ScaleTypes.JUMP_HEIGHT.getScaleData(livingEntity);
+            jump.setTargetScale(1.5f);
+            jump.setScaleTickDelay(jump.getScaleTickDelay());
+            ScaleData fall = ScaleTypes.FALLING.getScaleData(livingEntity);
+            fall.setTargetScale(0.4f);
+            fall.setScaleTickDelay(fall.getScaleTickDelay());
+            ScaleData step = ScaleTypes.STEP_HEIGHT.getScaleData(livingEntity);
+            step.setTargetScale(0.8f);
+            step.setScaleTickDelay(step.getScaleTickDelay());
+            ScaleData mining = ScaleTypes.MINING_SPEED.getScaleData(livingEntity);
+            mining.setTargetScale(2f);
+            mining.setScaleTickDelay(mining.getScaleTickDelay());
+        }
+        else
+        if (!hasMagic)
+        {
+            ScaleData height = ScaleTypes.HEIGHT.getScaleData(livingEntity);
+            height.setTargetScale(1f);
+            height.setScaleTickDelay(height.getScaleTickDelay());
+            ScaleData width = ScaleTypes.WIDTH.getScaleData(livingEntity);
+            width.setTargetScale(1f);
+            width.setScaleTickDelay(width.getScaleTickDelay());
+            ScaleData reach = ScaleTypes.BLOCK_REACH.getScaleData(livingEntity);
+            reach.setTargetScale(1f);
+            reach.setScaleTickDelay(reach.getScaleTickDelay());
+            ScaleData ereach = ScaleTypes.ENTITY_REACH.getScaleData(livingEntity);
+            ereach.setTargetScale(1f);
+            ereach.setScaleTickDelay(ereach.getScaleTickDelay());
+            ScaleData motion = ScaleTypes.MOTION.getScaleData(livingEntity);
+            motion.setTargetScale(1f);
+            motion.setScaleTickDelay(motion.getScaleTickDelay());
+            ScaleData jump = ScaleTypes.JUMP_HEIGHT.getScaleData(livingEntity);
+            jump.setTargetScale(1f);
+            jump.setScaleTickDelay(jump.getScaleTickDelay());
+            ScaleData fall = ScaleTypes.FALLING.getScaleData(livingEntity);
+            fall.setTargetScale(1f);
+            fall.setScaleTickDelay(fall.getScaleTickDelay());
+            ScaleData step = ScaleTypes.STEP_HEIGHT.getScaleData(livingEntity);
+            step.setTargetScale(1f);
+            step.setScaleTickDelay(step.getScaleTickDelay());
+            ScaleData mining = ScaleTypes.MINING_SPEED.getScaleData(livingEntity);
+            mining.setTargetScale(1f);
+            mining.setScaleTickDelay(mining.getScaleTickDelay());
+        }
+    }
 
     //TODO Make sure stats are fine tuned
-    //TODO Find a way to have the player smoothly transition into their new scales
     @Override
     public void onEquip(String identifier, int index, LivingEntity livingEntity, ItemStack stack) {
         ICurioItem.super.onEquip(identifier, index, livingEntity, stack);
-        ScaleData height = ScaleTypes.HEIGHT.getScaleData(livingEntity);
-        height.setScale(6.22f);
-        ScaleData width = ScaleTypes.WIDTH.getScaleData(livingEntity);
-        width.setScale(6.22f);
-        ScaleData reach = ScaleTypes.BLOCK_REACH.getScaleData(livingEntity);
-        reach.setScale(2f);
-        ScaleData ereach = ScaleTypes.ENTITY_REACH.getScaleData(livingEntity);
-        ereach.setScale(2f);
-        ScaleData motion = ScaleTypes.MOTION.getScaleData(livingEntity);
-        motion.setScale(3f);
-        ScaleData jump = ScaleTypes.JUMP_HEIGHT.getScaleData(livingEntity);
-        jump.setScale(6.22f);
-        ScaleData step = ScaleTypes.STEP_HEIGHT.getScaleData(livingEntity);
-        step.setScale(4f);
-        ScaleData mining = ScaleTypes.MINING_SPEED.getScaleData(livingEntity);
-        mining.setScale(2f);
+        if (!(livingEntity instanceof Player)) {
+            return;
+        }
+        Player player = (Player) livingEntity;
+        boolean hasMagic = MagicProvider.hasMagic(player, MagicCape.MANA_COST);
+        if (hasMagic) {
+            ScaleData height = ScaleTypes.HEIGHT.getScaleData(livingEntity);
+            height.setTargetScale(6.22f);
+            height.setScaleTickDelay(height.getScaleTickDelay());
+            ScaleData width = ScaleTypes.WIDTH.getScaleData(livingEntity);
+            width.setTargetScale(6.22f);
+            width.setScaleTickDelay(width.getScaleTickDelay());
+            ScaleData reach = ScaleTypes.BLOCK_REACH.getScaleData(livingEntity);
+            reach.setTargetScale(2f);
+            reach.setScaleTickDelay(reach.getScaleTickDelay());
+            ScaleData ereach = ScaleTypes.ENTITY_REACH.getScaleData(livingEntity);
+            ereach.setTargetScale(2f);
+            ereach.setScaleTickDelay(ereach.getScaleTickDelay());
+            ScaleData motion = ScaleTypes.MOTION.getScaleData(livingEntity);
+            motion.setTargetScale(3f);
+            motion.setScaleTickDelay(motion.getScaleTickDelay());
+            ScaleData jump = ScaleTypes.JUMP_HEIGHT.getScaleData(livingEntity);
+            jump.setTargetScale(1.5f);
+            jump.setScaleTickDelay(jump.getScaleTickDelay());
+            ScaleData fall = ScaleTypes.FALLING.getScaleData(livingEntity);
+            fall.setTargetScale(0.4f);
+            fall.setScaleTickDelay(fall.getScaleTickDelay());
+            ScaleData step = ScaleTypes.STEP_HEIGHT.getScaleData(livingEntity);
+            step.setTargetScale(0.8f);
+            step.setScaleTickDelay(step.getScaleTickDelay());
+            ScaleData mining = ScaleTypes.MINING_SPEED.getScaleData(livingEntity);
+            mining.setTargetScale(2f);
+            mining.setScaleTickDelay(mining.getScaleTickDelay());
+        }
     }
 
     @Override
     public void onUnequip(String identifier, int index, LivingEntity livingEntity, ItemStack stack) {
         ICurioItem.super.onUnequip(identifier, index, livingEntity, stack);
+
         ScaleData height = ScaleTypes.HEIGHT.getScaleData(livingEntity);
-        height.resetScale();
+        height.setTargetScale(1f);
+        height.setScaleTickDelay(height.getScaleTickDelay());
         ScaleData width = ScaleTypes.WIDTH.getScaleData(livingEntity);
-        width.resetScale();
+        width.setTargetScale(1f);
+        width.setScaleTickDelay(width.getScaleTickDelay());
         ScaleData reach = ScaleTypes.BLOCK_REACH.getScaleData(livingEntity);
-        reach.resetScale();
+        reach.setTargetScale(1f);
+        reach.setScaleTickDelay(reach.getScaleTickDelay());
         ScaleData ereach = ScaleTypes.ENTITY_REACH.getScaleData(livingEntity);
-        ereach.resetScale();
+        ereach.setTargetScale(1f);
+        ereach.setScaleTickDelay(ereach.getScaleTickDelay());
         ScaleData motion = ScaleTypes.MOTION.getScaleData(livingEntity);
-        motion.resetScale();
+        motion.setTargetScale(1f);
+        motion.setScaleTickDelay(motion.getScaleTickDelay());
         ScaleData jump = ScaleTypes.JUMP_HEIGHT.getScaleData(livingEntity);
-        jump.resetScale();
+        jump.setTargetScale(1f);
+        jump.setScaleTickDelay(jump.getScaleTickDelay());
+        ScaleData fall = ScaleTypes.FALLING.getScaleData(livingEntity);
+        fall.setTargetScale(1f);
+        fall.setScaleTickDelay(fall.getScaleTickDelay());
         ScaleData step = ScaleTypes.STEP_HEIGHT.getScaleData(livingEntity);
-        step.resetScale();
+        step.setTargetScale(1f);
+        step.setScaleTickDelay(step.getScaleTickDelay());
         ScaleData mining = ScaleTypes.MINING_SPEED.getScaleData(livingEntity);
-        mining.resetScale();
+        mining.setTargetScale(1f);
+        mining.setScaleTickDelay(mining.getScaleTickDelay());
     }
 
     @OnlyIn(Dist.CLIENT)
