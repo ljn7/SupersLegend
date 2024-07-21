fe9f014910aabb0e72867a7b0ccc5f1be88ca40c
ljn7 <johnnirmal.16897@gmail.com>
2024-07-21 23:57:08 +0530

    Postbox block changes with new postbox screen

---
+++ src/main/java/com/superworldsun/superslegend/client/handler/ClientModHandler.java
@@ -0,0 +1,2 @@
+package com.superworldsun.superslegend.client.handler;public class ClientModHandler {
+}
---
+++ src/main/java/com/superworldsun/superslegend/client/screen/PostboxScreen.java
@@ -0,0 +1,2 @@
+package com.superworldsun.superslegend.client.screen;public class PostboxScreen {
+}
