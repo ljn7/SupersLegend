87bf6d7700e69487fe9ee72a71115a5aea819892
ljn7 <johnnirmal.16897@gmail.com>
2024-10-15 06:55:34 +0530

    Fixed Boomerang and Implemented SeaBreezeBoomerang

--- src/main/java/com/superworldsun/superslegend/entities/projectiles/boomerang/SeaBreezeBoomerangEntity.java
+++ src/main/java/com/superworldsun/superslegend/entities/projectiles/boomerang/SeaBreezeBoomerangEntity.java
@@ -4,19 +4,26 @@ import com.superworldsun.superslegend.SupersLegendMain;
 import com.superworldsun.superslegend.registries.EntityTypeInit;
 import com.superworldsun.superslegend.registries.SoundInit;
 import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundSource;
+import net.minecraft.util.Mth;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.MobCategory;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
 import software.bernie.shadowed.eliotlash.mclib.utils.MathHelper;
 
 public class SeaBreezeBoomerangEntity extends AbstractBoomerangEntity {
+
     public SeaBreezeBoomerangEntity(EntityType<SeaBreezeBoomerangEntity> type, Level level) {
         super(type, level);
         turnBackTimer = 60;
         getDefaultSpeed = 1f;
-        turningBack = true;
+        turningBack = true; // Start with turning back enabled
     }
 
     public SeaBreezeBoomerangEntity(Player owner, ItemStack stack) {
@@ -28,23 +35,63 @@ public class SeaBreezeBoomerangEntity extends AbstractBoomerangEntity {
         return SoundInit.WW_BOOMERANG_FLY_LOOP.get();
     }
 
+    @Override
+    protected float getDamage() {
+        return 1f;
+    }
 
-    public void beforeTurnAround(Player player) {
-        // Follows where the entity is looking
-        {
-            double x = -MathHelper.wrapDegrees((player.yRotO * 3.141593F) / 180F);
-            double z = MathHelper.wrapDegrees((player.yRotO * 3.141593F) / 180F);
+    @Override
+    public void tick() {
+        Player owner = getOwner();
+        if (owner == null || owner.isDeadOrDying()) {
+            kill();
+            return;
+        }
 
-            double motionX = 0.5D * x * (double) MathHelper.wrapDegrees((player.yRotO / 180F) * 3.141593F);
-            double motionY = -0.5D * (double) MathHelper.wrapDegrees((player.yRotO / 180F) * 3.141593F);
-            double motionZ = 0.5D * z * (double) MathHelper.wrapDegrees((player.yRotO / 180F) * 3.141593F);
-            this.setDeltaMovement(motionX, motionY, motionZ);
+        if (tickCount % 4 == 0) {
+            level().playSound(null, getX(), getY(), getZ(), getFlyLoopSound(), SoundSource.PLAYERS, 1f, 1f);
+        }
+
+        updateCollision();
+        updateGaleMotion();
+        determineRotation();
+        prevRotation = getRotation();
+        setRotation(Mth.wrapDegrees(getRotation() + 36F));
+        updateCarriedItems();
+        super.tick();
+    }
+
+    private void updateGaleMotion() {
+        Player owner = getOwner();
+        if (owner == null) return;
+
+        Vec3 lookAngle = owner.getLookAngle();
+        double motionX = lookAngle.x * getDefaultSpeed;
+        double motionY = lookAngle.y * getDefaultSpeed;
+        double motionZ = lookAngle.z * getDefaultSpeed;
+
+        setDeltaMovement(motionX, motionY, motionZ);
+
+        double xPos = getX() + getDeltaMovement().x;
+        double yPos = getY() + getDeltaMovement().y;
+        double zPos = getZ() + getDeltaMovement().z;
+        setPos(xPos, yPos, zPos);
+
+        double distanceToOwner = distanceTo(owner);
+        if (distanceToOwner < 1.5D) {
+            onReturnToOwner();
         }
     }
 
     @Override
-    protected float getDamage() {
-        return 1f;
+    public void onEntityHit(Entity entity) {
+        Player owner = getOwner();
+        if (entity instanceof ItemEntity item) {
+            pickedItems.add(item);
+        } else if (entity instanceof LivingEntity && entity != owner) {
+            entity.hurt(getDamageSource(owner), getDamage());
+        }
+        // Note: We don't change the turnBackTimer here as the Gale boomerang always follows the player
     }
 
     public static EntityType<SeaBreezeBoomerangEntity> createEntityType() {
--- src/main/java/com/superworldsun/superslegend/network/message/SyncFreezeEffectMessage.java
+++ src/main/java/com/superworldsun/superslegend/network/message/SyncFreezeEffectMessage.java
@@ -1,4 +1,64 @@
 package com.superworldsun.superslegend.network.message;
 
-public class SyncFreezeEffectMessage {
+import com.superworldsun.superslegend.effect.FreezeEffect;
+import com.superworldsun.superslegend.registries.EffectInit;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.EffectInstance;
+import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.network.NetworkEvent;
+
+import javax.naming.Context;
+import java.io.IOException;
+import java.util.function.Supplier;
+
+public class SyncFreezeEffectMessage
+{
+    public int entityId;
+
+    public SyncFreezeEffectMessage(LivingEntity livingEntity)
+    {
+        entityId = livingEntity.getId();
+    }
+
+    private SyncFreezeEffectMessage()
+    {
+    }
+
+    public static SyncFreezeEffectMessage decode(FriendlyByteBuf buf)
+    {
+        SyncFreezeEffectMessage result = new SyncFreezeEffectMessage();
+        result.entityId = buf.readInt();
+        return result;
+    }
+
+    public void encode(FriendlyByteBuf buf)
+    {
+        buf.writeInt(entityId);
+    }
+
+    public static void receive(SyncFreezeEffectMessage message, Supplier<NetworkEvent.Context> ctxSupplier)
+    {
+        NetworkEvent.Context ctx = ctxSupplier.get();
+        ctx.setPacketHandled(true);
+        ctx.enqueueWork(() -> DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> SyncFreezeEffectMessage.handlePacket(message, ctx)));
+    }
+
+    @OnlyIn(Dist.CLIENT)
+    public static void handlePacket(SyncFreezeEffectMessage message, NetworkEvent.Context ctx) {
+        Minecraft client = Minecraft.getInstance();
+        Level level = client.level;
+        if (level != null) {
+            Entity entity = level.getEntity(message.entityId);
+            if (entity != null)
+                ((LivingEntity) entity).addEffect(new MobEffectInstance(EffectInit.FREEZE.get(), 2));
+        }
+
+    }
 }
